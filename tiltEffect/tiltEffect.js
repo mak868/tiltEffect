f1f96cc8d686468bfccc8db87f7e5029fcd1bb28
Salvatore Gentile <salgnt@outlook.com>
2014-08-26 16:21:47 +0200

    Minor fixes

--- tiltEffect/tiltEffect.js
+++ tiltEffect/tiltEffect.js
@@ -1,43 +1,45 @@
 /*
  * tiltEffect: Metro Tiles Tilt Effect
  * URL: http://github.com/SalGnt/tiltEffect
- * LICENSE: The MIT License (MIT) 
+ * LICENSE: The MIT License (MIT)
  */
 
 (function($) {
-	$.fn.tiltEffect = function() {
-		this.mousemove(function(e) {
-			// Cursor position inside the element.
-			var x = e.pageX - this.offsetLeft;
-			var y = e.pageY - this.offsetTop;
+    $.fn.tiltEffect = function() {
+        this.mousemove(function(e) {
+            // Cursor position inside the element.
+            var x = e.pageX - this.offsetLeft;
+            var y = e.pageY - this.offsetTop;
 
-			var quarterWidth = $(this).width() / 4;
-			var quarterHeight = $(this).height() / 4;
+            var width = $(this).width();
+            var height = $(this).height();
+            var quarterWidth = width / 4;
+            var quarterHeight = height / 4;
 
-			if (y < quarterHeight && x > quarterWidth && x < quarterWidth*3) {
-				setEffect(this, "top");
-			} else if (x > quarterWidth*3 && y > quarterHeight && y < quarterHeight*3) {
-				setEffect(this, "right");
-			} else if (y > quarterHeight*3 && x > quarterWidth && x < quarterWidth*3) {
-				setEffect(this, "bottom");
-			} else if (x < quarterWidth && y > quarterHeight && y < quarterHeight*3) {
-				setEffect(this, "left");
-			} else {
-				setEffect(this, "center");
-			}
-		});
+            if (y < quarterHeight && x > 0 && x < width) {
+                setEffect(this, "top");
+            } else if (y > quarterHeight*3 && x > 0 && x < width) {
+                setEffect(this, "bottom");
+            } else if (x > quarterWidth*3 && y > 0 && y < height) {
+                setEffect(this, "right");
+            } else if (x < quarterWidth && y > 0 && y < height) {
+                setEffect(this, "left");
+            } else {
+                setEffect(this, "center");
+            }
+        });
 
- 		function setEffect(targetElement, effectDirection) {
- 			var directions = new Array("center", "top", "right", "bottom", "left");
+        function setEffect(targetElement, effectDirection) {
+            var directions = new Array("center", "top", "right", "bottom", "left");
 
-			// Disable all the effect except the right one.
-			for (i in directions) {
-				if (!(directions[i] == effectDirection)) {
-					$(targetElement).removeClass(directions[i]);
-				}
-			}
+            // Disable all the effect except the right one.
+            for (i in directions) {
+                if (!(directions[i] == effectDirection)) {
+                    $(targetElement).removeClass(directions[i]);
+                }
+            }
 
-			$(targetElement).addClass(effectDirection);
-		}
-	}
+            $(targetElement).addClass(effectDirection);
+        }
+    }
 }(jQuery));
